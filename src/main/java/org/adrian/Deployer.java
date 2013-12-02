a263f998b784065c6bbe3019124550467092e7c2
aosullivan <adrian.osullivan@gmail.com>
2013-12-02 16:17:09 -0500

    Pause for results

--- src/main/java/org/adrian/Deployer.java
+++ src/main/java/org/adrian/Deployer.java
@@ -1,14 +1,21 @@
 package org.adrian;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import com.amazonaws.services.elasticmapreduce.model.DescribeJobFlowsRequest;
+import com.amazonaws.services.elasticmapreduce.model.DescribeJobFlowsResult;
 import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
+import com.amazonaws.services.elasticmapreduce.model.JobFlowDetail;
+import com.amazonaws.services.elasticmapreduce.model.JobFlowExecutionState;
 import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
 import com.amazonaws.services.elasticmapreduce.model.PlacementType;
 import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
@@ -26,17 +33,36 @@ public class Deployer {
     private static final String mr_main_class = "org.adrian.WordCount";
     private static final String mr_cmdline_arg1 = "s3n://poobar/hamlet111.txt";
     private static final String mr_cmdline_arg2 = "s3n://poobar/outputs/"+jobFlowName+"/"+stepname+"/";
-
+    
+    private static final List<JobFlowExecutionState> DONE_STATES = Arrays
+            .asList(new JobFlowExecutionState[] { JobFlowExecutionState.COMPLETED,
+                                                 JobFlowExecutionState.FAILED,
+                                                 JobFlowExecutionState.TERMINATED });
+        
     public static void main(String[] args) {
 
         AWSCredentials credentials = new BasicAWSCredentials("AKIAJX23WFZA5737VMVQ", "3FhWX0tK8jWV7Lxi70OKy5R7AAF2VwxA3WTj7sf0");
-        AmazonElasticMapReduce service = new AmazonElasticMapReduceClient(credentials);
+        ClientConfiguration config = new ClientConfiguration()
+                                            .withProxyHost("surf-proxy.intranet.db.com")
+                                            .withProxyPort(8080);
+        AmazonElasticMapReduce service = new AmazonElasticMapReduceClient(credentials, config);
         
         RunJobFlowRequest request = jobFlowRequest();
         RunJobFlowResult result = service.runJobFlow(request);
         
-        result.toString();
+        waitForResult(service, result);
+        
+    }
 
+
+    private static DescribeJobFlowsRequest describeResult(RunJobFlowResult result) {
+        return new DescribeJobFlowsRequest(Arrays.asList(new String[] { result.getJobFlowId() }));
+    }
+    
+    public static boolean isDone(String value)
+    {
+        JobFlowExecutionState state = JobFlowExecutionState.fromValue(value);
+        return DONE_STATES.contains(state);
     }
 
     private static StepConfig stepConfig(final String stepname, HadoopJarStepConfig jarsetup) {
@@ -82,13 +108,44 @@ public class Deployer {
 
     private static JobFlowInstancesConfig jobFlowInstance() {
         JobFlowInstancesConfig conf = new JobFlowInstancesConfig();
-        conf.setEc2KeyName("class");
-        conf.setInstanceCount(11);
+        //conf.setEc2KeyName("class");
+        conf.setInstanceCount(5);
         conf.setKeepJobFlowAliveWhenNoSteps(true);
         conf.setMasterInstanceType("m1.small");
-        conf.setPlacement(new PlacementType("us-east-1"));
+        conf.setPlacement(new PlacementType("us-east-1a"));
         conf.setSlaveInstanceType("m1.small");
         return conf;
     }
+    
+    private static void pause() {
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
+    private static void waitForResult(AmazonElasticMapReduce service, RunJobFlowResult result) {
+        //Check the status of the running job
+        String lastState = "";
+        STATUS_LOOP: while (true) {
+            DescribeJobFlowsResult descResult = service.describeJobFlows(describeResult(result));
+            
+            for (JobFlowDetail detail : descResult.getJobFlows()) {
+                String state = detail.getExecutionStatusDetail().getState();
+                
+                if (isDone(state)) {
+                    System.out.println("Job " + state + ": " + detail.toString());
+                    break STATUS_LOOP;
+                }
+                else if (!lastState.equals(state)) {
+                    lastState = state;
+                    System.out.println("Job " + state + " at " + new Date().toString());
+                }
+            }
+            
+            pause();
+        }
+    }
+    
 }
