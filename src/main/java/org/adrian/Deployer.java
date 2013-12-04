e30722cea0a30bf5a3077ea8713b4d9bfa1f7173
aosullivan <adrian.osullivan@gmail.com>
2013-12-04 16:50:32 -0500

    Working version

--- src/main/java/org/adrian/Deployer.java
+++ src/main/java/org/adrian/Deployer.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import com.amazonaws.ClientConfiguration;
@@ -23,6 +22,7 @@ import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
 import com.amazonaws.services.elasticmapreduce.model.PlacementType;
 import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
 import com.amazonaws.services.elasticmapreduce.model.RunJobFlowResult;
+import com.amazonaws.services.elasticmapreduce.model.ScriptBootstrapActionConfig;
 import com.amazonaws.services.elasticmapreduce.model.StepConfig;
 import com.amazonaws.services.elasticmapreduce.util.BootstrapActions;
 import com.amazonaws.services.elasticmapreduce.util.BootstrapActions.Daemon;
@@ -31,17 +31,17 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 
 public class Deployer {
+    private static final String id = new Date().toString();
     
     private static final String s3_jar_file = "s3n://poobar/wordcount.jar";
     private static final String s3_log_folder = "s3n://poobar/logs";
     
-    private static final String jobFlowName = "WordCount-job-flow-" + new Date().toString();
+    private static final String jobFlowName = "WordCount-job-flow-" + id;
     private static final String stepname = "Step" + System.currentTimeMillis();
     
-    private static final UUID RANDOM_UUID = UUID.randomUUID();
     private static final String mr_main_class = "org.adrian.WordCount";
     private static final String mr_cmdline_arg1 = "s3n://poobar/hamlet111.txt";
-    private static final String mr_cmdline_arg2 = "s3://poobar/outputs/"+RANDOM_UUID.toString() + "/";
+    private static final String mr_cmdline_arg2 = "s3://poobar/outputs/"+id + "/";
     
     private static final List<JobFlowExecutionState> DONE_STATES = Arrays
             .asList(new JobFlowExecutionState[] { JobFlowExecutionState.COMPLETED,
@@ -66,7 +66,7 @@ public class Deployer {
 
 
     private static void downloadResult(AWSCredentials credentials, ClientConfiguration config) {
-        String outputFile = "outputs/"+RANDOM_UUID.toString()+"/part-r-00000";
+        String outputFile = "outputs/"+id+"/part-r-00000";
         String local = "target/results.out";
 
         AmazonS3 conn = new AmazonS3Client(credentials, config);
@@ -90,7 +90,8 @@ public class Deployer {
 
     private static StepConfig stepConfig(final String stepname) {
         return new StepConfig()
-            .withActionOnFailure("TERMINATE_JOB_FLOW")
+            //.withActionOnFailure("TERMINATE_JOB_FLOW")
+            .withActionOnFailure("CANCEL_AND_WAIT")
             .withHadoopJarStep(hadoopJarStepConfig(argumentsAsList(jobFlowName, stepname)))
             .withName(stepname);
     }
@@ -116,27 +117,44 @@ public class Deployer {
     }
 
     private static RunJobFlowRequest jobFlowRequest() {
-        BootstrapActions bootstrapActions = new BootstrapActions();
+        
         return new RunJobFlowRequest()
-            .withName(jobFlowName)
-            .withBootstrapActions(bootstrapActions.newRunIf(
-               "instance.isMaster=true", action(bootstrapActions)))
+            .withName(jobFlowName)            
+            //.withBootstrapActions(scriptAction("s3://elasticmapreduce/samples/spark/0.7.3/install-spark-shark.sh", null), jobTrackerHeapSizeAction(2048))
             .withInstances(jobFlowInstance())
             .withLogUri(s3_log_folder)
             .withSteps(asList(stepConfig(stepname)));
     }
 
-
-    private static BootstrapActionConfig action(BootstrapActions bootstrapActions) {
-        return bootstrapActions.newConfigureDaemons()
-               .withHeapSize(Daemon.JobTracker, 2048)
-               .build();
+    private static BootstrapActionConfig jobTrackerHeapSizeAction(int heap) {
+        BootstrapActions bootstrapActions = new BootstrapActions();
+        return bootstrapActions.newRunIf("instance.isMaster=true", bootstrapActions.newConfigureDaemons()
+                .withHeapSize(Daemon.JobTracker, heap)
+                .build());
     }
 
+    private static BootstrapActionConfig scriptAction(String bootstrapPath, List<String> args) {
+        BootstrapActionConfig bootstrapConfig = new BootstrapActionConfig();
+        ScriptBootstrapActionConfig bootstrapScriptConfig = new ScriptBootstrapActionConfig();
+        
+        bootstrapScriptConfig.setPath(bootstrapPath);
+
+        if (args != null) {
+            bootstrapScriptConfig.setArgs(args);
+        }
+
+        bootstrapConfig.setName("Custom script action");
+        bootstrapConfig.setScriptBootstrapAction(bootstrapScriptConfig);
+
+        return bootstrapConfig;
+    }   
+    
+    
     private static JobFlowInstancesConfig jobFlowInstance() {
         JobFlowInstancesConfig conf = new JobFlowInstancesConfig()
             .withInstanceCount(3)
             .withHadoopVersion("0.20.205")
+            //.withEc2KeyName("adrian")            
             .withKeepJobFlowAliveWhenNoSteps(false)
             .withMasterInstanceType("m1.small")
             .withPlacement(new PlacementType("us-east-1a"))
---
+++ src/main/java/org/adrian/Uploader.java
@@ -0,0 +1,67 @@
+package org.adrian;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressListener;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+
+public class Uploader {
+    
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        AWSCredentials credentials = new BasicAWSCredentials("AKIAJX23WFZA5737VMVQ", "3FhWX0tK8jWV7Lxi70OKy5R7AAF2VwxA3WTj7sf0");
+        ClientConfiguration config = new ClientConfiguration()
+                                            .withProxyHost("surf-proxy.intranet.db.com")
+                                            .withProxyPort(8080);
+        
+        upload(credentials, config);
+    }
+
+
+    private static void upload(AWSCredentials credentials, ClientConfiguration config) throws InterruptedException, IOException {
+        String filename = "e:\\data\\shak.txt";
+        String existingBucketName = "poobar";
+        String keyName = "shakespeare.txt";
+        File file = new File(filename);
+
+        AmazonS3Client conn = new AmazonS3Client(credentials, config);
+        //conn.putObject(new PutObjectRequest(existingBucketName, keyName, file));
+        PutObjectRequest request = new PutObjectRequest(existingBucketName, keyName, file);
+                
+        TransferManager tm = new TransferManager(conn);
+        //TransferManagerConfiguration tfc = new TransferManagerConfiguration();
+        //System.err.println(tfc.getMinimumUploadPartSize());
+        //tfc.setMinimumUploadPartSize(655360);
+        //tfc.setMultipartUploadThreshold(1024);
+        //tm.setConfiguration(tfc);
+
+        request.setGeneralProgressListener(new ProgressListener() {
+            public void progressChanged(ProgressEvent event) {
+                if (event.getEventCode() > 0) 
+                    System.out.println("Transferred bytes: " + 
+                        event.getBytesTransferred() + " Code: " +  event.getEventCode());
+            }
+        });
+        
+        Upload upload = tm.upload(request);
+        
+        try {
+            upload.waitForCompletion();
+        } catch (AmazonClientException amazonClientException) {
+            System.out.println("Unable to upload file, upload was aborted.");
+            amazonClientException.printStackTrace();
+        }
+        
+        
+    }
+
+}
