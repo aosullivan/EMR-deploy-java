4d83f4b86b1c8ef660cea911054d8e698b0ffd61
aosullivan <adrian.osullivan@gmail.com>
2013-12-08 13:47:48 -0500

    Failed attempt to run s3distcp

--- src/main/java/org/adrian/Deployer.java
+++ src/main/java/org/adrian/Deployer.java
@@ -12,7 +12,6 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
-import com.amazonaws.services.elasticmapreduce.model.BootstrapActionConfig;
 import com.amazonaws.services.elasticmapreduce.model.DescribeJobFlowsRequest;
 import com.amazonaws.services.elasticmapreduce.model.DescribeJobFlowsResult;
 import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
@@ -22,27 +21,28 @@ import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
 import com.amazonaws.services.elasticmapreduce.model.PlacementType;
 import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
 import com.amazonaws.services.elasticmapreduce.model.RunJobFlowResult;
-import com.amazonaws.services.elasticmapreduce.model.ScriptBootstrapActionConfig;
 import com.amazonaws.services.elasticmapreduce.model.StepConfig;
-import com.amazonaws.services.elasticmapreduce.util.BootstrapActions;
-import com.amazonaws.services.elasticmapreduce.util.BootstrapActions.Daemon;
-import com.amazonaws.services.elasticmapreduce.util.StepFactory;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 
 public class Deployer {
+
     private static final String id = new Date().toString();
     
     private static final String s3_jar_file = "s3n://poobar/wordcount.jar";
+    private static final String distcp_jar_file = "/home/hadoop/lib/emr-s3distcp-1.0.jar";
     private static final String s3_log_folder = "s3n://poobar/logs";
     
     private static final String jobFlowName = "WordCount-job-flow-" + id;
-    private static final String stepname = "Step" + System.currentTimeMillis();
     
     private static final String mr_main_class = "org.adrian.WordCount";
-    private static final String mr_cmdline_arg1 = "s3n://poobar/shak2.txt"; //input
-    private static final String mr_cmdline_arg2 = "s3://poobar/outputs/"+id + "/";  //output
+
+    private static final String hdfs_input = "hfds://root/adrian";
+    private static final String hdfs_output = "hfds://root/adrian/outputs";
+    //private static final String mr_cmdline_arg1 = "s3n://poobar/shak2.txt"; //input
+    private static final String s3_input = "s3n://poobar/hamlet111.txt"; //input
+    private static final String s3_output = "s3://poobar/outputs/"+id + "/";  //output
     
     public static void main(String[] args) {
 
@@ -84,33 +84,59 @@ public class Deployer {
         return DONE_STATES.contains(state);
     }
 
-    private static StepConfig stepConfig(final String stepname) {
-        return new StepConfig()
-            .withActionOnFailure("TERMINATE_JOB_FLOW")
-            //.withActionOnFailure("CANCEL_AND_WAIT")
-            .withHadoopJarStep(new StepFactory().newEnableDebuggingStep())
-            .withHadoopJarStep(hadoopJarStepConfig(argumentsAsList(jobFlowName, stepname)))
-            .withName(stepname);
+    private static List<StepConfig> steps() {
+        List<StepConfig> steps = new LinkedList<StepConfig>();
+        steps.add(uploadStep());
+        steps.add(mapReduceStep());
+        steps.add(downloadStep());        
+        return steps;
     }
 
-    private static HadoopJarStepConfig hadoopJarStepConfig(List<String> arguments) {
+    private static StepConfig downloadStep() {
+        return new StepConfig()
+            .withName("DistCpDownload")
+            .withActionOnFailure("TERMINATE_JOB_FLOW")
+            .withHadoopJarStep(new HadoopJarStepConfig(distcp_jar_file)
+            .withMainClass("com.amazon.elasticmapreduce.s3distcp.Main")
+            .withArgs("--src", hdfs_output, "--dest", s3_output));
+    }
+
+    private static StepConfig mapReduceStep() {
+        return  new StepConfig()
+            .withActionOnFailure("TERMINATE_JOB_FLOW")
+            .withHadoopJarStep(hadoopJarStepConfig(s3_jar_file, argumentsAsList(s3_input, hdfs_output), mr_main_class))
+            .withName("MRStep" + System.currentTimeMillis());
+    }
+
+    private static StepConfig uploadStep() {
+        return  new StepConfig()
+            .withName("DistCpUpload")
+            .withActionOnFailure("TERMINATE_JOB_FLOW")
+            .withHadoopJarStep(new HadoopJarStepConfig(distcp_jar_file)
+            .withMainClass("com.amazon.elasticmapreduce.s3distcp.Main")
+            .withArgs("--src", s3_input, "--dest", hdfs_input));
+        
+    }
+
+    private static HadoopJarStepConfig hadoopJarStepConfig(String jar, List<String> arguments, String mainClass) {
         return new HadoopJarStepConfig()
             .withArgs(arguments)
-            .withJar(s3_jar_file)
-            .withMainClass(mr_main_class);
+            .withJar(jar)
+            .withMainClass(mainClass);
+    }
+    
+    private static HadoopJarStepConfig hadoopJarStepConfig(String jar, List<String> arguments) {
+        return new HadoopJarStepConfig()
+            .withArgs(arguments)
+            .withJar(jar);
     }
 
-    private static List<String> argumentsAsList(final String jobFlowName, final String stepname) {
-        List<String> arguments = new LinkedList<String>();
-        arguments.add(mr_cmdline_arg1);
-        arguments.add(mr_cmdline_arg2);
-        return arguments;
-    }
-
-    private static List<StepConfig> asList(StepConfig stepConfig) {
-        List<StepConfig> steps = new LinkedList<StepConfig>();
-        steps.add(stepConfig);
-        return steps;
+    private static List<String> argumentsAsList(final String... args) {
+        List<String> argList = new LinkedList<String>();
+        for (String arg : args){
+            argList.add(arg);
+        }        
+        return argList;
     }
 
     private static RunJobFlowRequest jobFlowRequest() {
@@ -118,44 +144,44 @@ public class Deployer {
         return new RunJobFlowRequest()
             .withName(jobFlowName)            
             //.withBootstrapActions(scriptAction("s3://poobar/install-spark-shark.sh", null))
-            .withInstances(jobFlowInstance())
+            .withInstances(jobFlowInstances())
             .withLogUri(s3_log_folder)
-            .withSteps(asList(stepConfig(stepname)));
+            .withSteps(steps());
     }
 
-    private static BootstrapActionConfig jobTrackerHeapSizeAction(int heap) {
-        BootstrapActions bootstrapActions = new BootstrapActions();
-        return bootstrapActions.newRunIf("instance.isMaster=true", bootstrapActions.newConfigureDaemons()
-                .withHeapSize(Daemon.JobTracker, heap)
-                .build());
-    }
-
-    private static BootstrapActionConfig scriptAction(String bootstrapPath, List<String> args) {
-        BootstrapActionConfig bootstrapConfig = new BootstrapActionConfig();
-        ScriptBootstrapActionConfig bootstrapScriptConfig = new ScriptBootstrapActionConfig();
-        
-        bootstrapScriptConfig.setPath(bootstrapPath);
-
-        if (args != null) {
-            bootstrapScriptConfig.setArgs(args);
-        }
-
-        bootstrapConfig.setName("Custom script action");
-        bootstrapConfig.setScriptBootstrapAction(bootstrapScriptConfig);
-
-        return bootstrapConfig;
-    }   
+//    private static BootstrapActionConfig jobTrackerHeapSizeAction(int heap) {
+//        BootstrapActions bootstrapActions = new BootstrapActions();
+//        return bootstrapActions.newRunIf("instance.isMaster=true", bootstrapActions.newConfigureDaemons()
+//                .withHeapSize(Daemon.JobTracker, heap)
+//                .build());
+//    }
+//
+//    private static BootstrapActionConfig scriptAction(String bootstrapPath, List<String> args) {
+//        BootstrapActionConfig bootstrapConfig = new BootstrapActionConfig();
+//        ScriptBootstrapActionConfig bootstrapScriptConfig = new ScriptBootstrapActionConfig();
+//        
+//        bootstrapScriptConfig.setPath(bootstrapPath);
+//
+//        if (args != null) {
+//            bootstrapScriptConfig.setArgs(args);
+//        }
+//
+//        bootstrapConfig.setName("Custom script action");
+//        bootstrapConfig.setScriptBootstrapAction(bootstrapScriptConfig);
+//
+//        return bootstrapConfig;
+//    }   
     
     
-    private static JobFlowInstancesConfig jobFlowInstance() {
+    private static JobFlowInstancesConfig jobFlowInstances() {
         JobFlowInstancesConfig conf = new JobFlowInstancesConfig()
-            .withInstanceCount(13)
+            .withInstanceCount(2)
             .withHadoopVersion("0.20.205")
             .withEc2KeyName("adrian")            
             .withKeepJobFlowAliveWhenNoSteps(false)
-            .withMasterInstanceType("m1.large")
+            .withMasterInstanceType("m1.small")
             .withPlacement(new PlacementType("us-east-1a"))
-            .withSlaveInstanceType("m1.large");
+            .withSlaveInstanceType("m1.small");
         return conf;
     }
     
@@ -178,7 +204,7 @@ public class Deployer {
                 
                 if (isDone(state)) {
                     System.out.println("Job " + state + ": " + detail.toString());
-                    System.out.println("Output folder: " + mr_cmdline_arg2);
+                    System.out.println("Output folder: " + s3_output);
                     break DONE;
                 }
                 else if (!lastState.equals(state)) {
